f496f7db461fc1246dca291e7dc79603743a073f
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-01-29 02:27:36 +0900

    feat: 기본 경로 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+    </Routes>
+  );
 }
 
 export default App;
