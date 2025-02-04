350f9b4dcb5ce959a75a121e6940d35ead94610c
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-02-05 01:17:34 +0900

    feat: Login에 필요한 컴포넌트 구현

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,10 +1,20 @@
 import styled from "@emotion/styled";
 
 const InputBox = styled.input`
-    width: 358px;
-    height: 47px;
+  width: 358px;
+  height: 47px;
 
-    font-size
-`
+  font-size: 16px;
+  font-weight: 400;
+
+  color: #000000;
+  background-color: #f1f1f1;
+
+  border: 1px solid #e6e6e6;
+  border-radius: 6px;
+  box-sizing: border-box;
+
+  padding: 14px 16px;
+`;
 
 export default InputBox;
