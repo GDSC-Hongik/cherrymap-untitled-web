ea59d9f5910b9bb5f9985a63117655db2eb81ebf
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-02-05 00:54:39 +0900

    feat: GlobalStyle 폰트 설정

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+const InputBox = styled.input`
+    width: 358px;
+    height: 47px;
+
+    font-size
+`
+
+export default InputBox;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Global } from "@emotion/react";
+import GlobalStyle from "./utils/GlobalStyle.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Global css={GlobalStyle} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
