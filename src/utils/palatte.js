273faa21c92b0fa24545cc930fa672ea7009ed49
Hyeok Lee <c235335@g.hongik.ac.kr>
2025-02-20 01:21:33 +0900

    feat: 디자인 에셋, 토큰 등록

---
+++ src/utils/palatte.js
@@ -0,0 +1,25 @@
+export const palette = {
+  primary: {
+    default: "#ff2355",
+    depart: "#1399d2",
+    arrive: "#dd4040",
+  },
+
+  transfortation: {
+    bus: "#373e51",
+    townbus: "#1cb10b",
+    walk: "#ff85a0",
+    subway: {
+      exit: "#ffc831",
+      line1: "#0032a0",
+      line2: "#00b140",
+      line3: "#fc4c02",
+      line4: "00a9e0",
+      line5: "#a05eb5",
+      line6: "#a9431e",
+      line7: "#67823a",
+      line8: "#e31c79",
+      line9: "#8c8279",
+    },
+  },
+};
